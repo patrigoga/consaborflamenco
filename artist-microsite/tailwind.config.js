fbca418845e1f40dbac2af6dea0e7a71eff65785
Patricio Gómez García <patrigoga@gmail.com>
2026-07-01 15:02:37 +0200

    Add Docker support, Next.js config, Tailwind and app bootstrap

---
+++ artist-microsite/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: '#8B1F2D',
+          gold: '#C79A43',
+          navy: '#1E2A33',
+          warm: '#F6F0E6'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
